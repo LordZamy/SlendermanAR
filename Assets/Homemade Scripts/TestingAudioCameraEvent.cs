e6083904a30658605cf5e5c5a16fe3de94242feb
Nicholas Joaquin <nicholas.joaquin@outlook.com>
2016-11-12 17:52:26 -0600

    adding shooters as well as a skin

--- Assets/Homemade Scripts/TestingAudioCameraEvent.cs
+++ Assets/Homemade Scripts/TestingAudioCameraEvent.cs
@@ -7,14 +7,17 @@ public class TestingAudioCameraEvent : MonoBehaviour {
     public int hasBeenActivated = 0;
     public float previousTime = 0;
     public float newTime = 0;
+	public GameObject objectLook;
+	public GameObject myLook;
     void Start() {
         sounds = GetComponent<AudioSource>();
         voiceClips = Resources.LoadAll<AudioClip>("Sound");
         noise1 = voiceClips[0];
+		objectLook = GameObject.Find("EthanBody");
+		myLook = GameObject.Find("Tango AR Camera");
     }
     void Update() {
-        var objectLook = GameObject.Find("EthanBody");
-        var myLook = GameObject.Find("Tango AR Camera");
+
         Vector3 dirFromMeToObject = (objectLook.transform.position - myLook.transform.position).normalized;
         Vector3 myCurrentFacingDir = transform.forward;
         if (Vector3.Dot(dirFromMeToObject, myCurrentFacingDir) > 0.75 && hasBeenActivated == 0) {
