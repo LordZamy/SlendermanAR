b05e3ef79a8fcb46f59de875adc94b6578575866
Nicholas Joaquin <nicholas.joaquin@outlook.com>
2016-11-13 02:23:19 -0600

    ugh

--- Assets/Homemade Scripts/TestingAudioCameraEvent.cs
+++ Assets/Homemade Scripts/TestingAudioCameraEvent.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 public class TestingAudioCameraEvent : MonoBehaviour {
     public AudioClip[] voiceClips;
     public AudioClip noise1;
@@ -13,6 +14,11 @@ public class TestingAudioCameraEvent : MonoBehaviour {
 	public Button shootBullet;
 	private int trumpLives;
 	public Text howManyLives;
+	public Text howMuchFilm;
+	private int filmCount;
+	public GameObject deletTrump;
+	public GameObject deletAudioTrump;
+	public GameObject winScreen;
     void Start() {
         sounds = GetComponent<AudioSource>();
         voiceClips = Resources.LoadAll<AudioClip>("Sound");
@@ -23,6 +29,7 @@ public class TestingAudioCameraEvent : MonoBehaviour {
 		btn.onClick.AddListener (TaskOnClick);
 		trumpLives = 3;
 		howManyLives.text = trumpLives.ToString ();
+		filmCount = Convert.ToInt32(howMuchFilm.text);
     }
     void Update() {
 
@@ -30,7 +37,7 @@ public class TestingAudioCameraEvent : MonoBehaviour {
         Vector3 myCurrentFacingDir = transform.forward;
         if (Vector3.Dot(dirFromMeToObject, myCurrentFacingDir) > 0.75 && hasBeenActivated == 0) {
             Debug.Log("object is with a 45 degree arc in front of us");
-            noise1 = voiceClips[Random.Range(0, voiceClips.Length)];
+            noise1 = voiceClips[UnityEngine.Random.Range(0, voiceClips.Length)];
             sounds.PlayOneShot(noise1, 1);
             hasBeenActivated =1;
             previousTime = Time.time;
@@ -47,10 +54,21 @@ public class TestingAudioCameraEvent : MonoBehaviour {
 	void TaskOnClick () {
 		Vector3 dirFromMeToObject = (objectLook.transform.position - myLook.transform.position).normalized;
 		Vector3 myCurrentFacingDir = transform.forward;
-		if (Vector3.Dot(dirFromMeToObject, myCurrentFacingDir) > 0.95) {
-			Debug.Log("object is narrow!");
+		filmCount = Convert.ToInt32(howMuchFilm.text);
+		if (Vector3.Dot (dirFromMeToObject, myCurrentFacingDir) > 0.95 && filmCount != 0) {
+			Debug.Log ("object is narrow!");
 			trumpLives = trumpLives - 1;
+			filmCount = filmCount - 1;
 			howManyLives.text = trumpLives.ToString ();
+			howMuchFilm.text = filmCount.ToString ();
+			if (filmCount == 0) {
+				deletTrump.SetActive (false);
+				deletAudioTrump.SetActive (false);
+				winScreen.SetActive (true);
+			}
+		} else if (filmCount != 0) {
+			filmCount = filmCount - 1;
+			howMuchFilm.text = filmCount.ToString ();
 		}
 	}
  }
